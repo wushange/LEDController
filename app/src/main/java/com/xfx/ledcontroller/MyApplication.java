65f3486e56ad559439e92e03d0cb5203c7eaa5bd
wushange <309646776@qq.com>
2018-07-13 16:30:03 +0800

    init

---
+++ app/src/main/java/com/xfx/ledcontroller/MyApplication.java
@@ -0,0 +1,26 @@
+package com.xfx.ledcontroller;
+
+import android.app.Application;
+
+import com.xfx.ledcontroller.ui.base.BaseApplication;
+
+/**
+ * @author wushange QQ:309646776
+ * @name LEDController
+ * @class name：com.xfx.ledcontroller
+ * @class describe
+ * @time 2018/7/13/0013 13:59
+ * @class describe
+ */
+public class MyApplication extends BaseApplication {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+	@Override
+	public void exit() {
+
+	}
+}
---
+++ app/src/main/java/com/xfx/ledcontroller/ui/a.java
@@ -0,0 +1,12 @@
+package com.xfx.ledcontroller.ui;
+
+/**
+ * @author wushange QQ:309646776
+ * @name LEDController
+ * @class name：com.xfx.ledcontroller.ui
+ * @class describe
+ * @time 2018/7/13/0013 14:09
+ * @class describe
+ */
+public class a {
+}
